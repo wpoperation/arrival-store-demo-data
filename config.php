9ba0c0706ce7a174b0c6ec4b15753d9ac229d073
sagar <girisagar051@gmail.com>
2020-05-05 10:20:19 +0545

    initial commit | store demo content added

---
+++ config.php
@@ -0,0 +1,400 @@
+<?php
+
+$data = array(
+
+	'furniture-store' => array(
+		'categories'        => array( 'Furniture Store','Store' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/furniture-store/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'top',
+			'shop'      => 'Shop Menus'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+	'corporate' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'slider_file'       => 'slider.ss3',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				array(
+					'slug'    => 'smart-slider-3',
+					'init'    => 'smart-slider-3/smart-slider-3.php',
+					'name'    => 'Smart Slider',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+
+	'construction' => array(
+		'categories'        => array( 'Corporate','Construction' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-two/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				
+			),
+		),
+	),
+
+	'restaurant' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-three/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+			
+			),
+		),
+	),
+
+
+	'education' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-four/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'slider_file'       => 'slider.ss3',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				array(
+					'slug'    => 'smart-slider-3',
+					'init'    => 'smart-slider-3/smart-slider-3.php',
+					'name'    => 'Smart Slider',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+
+	'fitness' => array(
+		'categories'        => array( 'Fitness','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-one/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+	'spa' => array(
+		'categories'        => array( 'Spa'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/spa/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Beauty Spa',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> true,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+				array(
+					'slug'    => 'woocommerce',
+					'init'    => 'woocommerce/woocommerce.php',
+					'name'    => 'WooCommerce',
+				),
+			
+			),
+		),
+	),
+
+	'portfolio' => array(
+		'categories'        => array( 'Portfolio'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/portfolio/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+
+	'travel' => array(
+		'categories'        => array( 'Travel'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/travel/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Travel',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+			
+			),
+		),
+	),
+
+	'charity' => array(
+		'categories'        => array( 'Charity' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/charity',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+	'app' => array(
+		'categories'        => array( 'App','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/app',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+);
